8e512242c2f1afcb297e95cebb4240d90e99ce9b
chenwochong <chenwochong@163.com>
2026-02-11 18:36:56 +0800

    add skaffold

--- main.go
+++ main.go
@@ -10,6 +10,12 @@ import (
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello from Ko! Running on %s/%s\n", runtime.GOOS, runtime.GOARCH)
 	fmt.Fprintf(w, "Pod Name: %s\n", os.Getenv("POD_NAME"))
+	config, err := os.ReadFile("/var/run/ko/config.txt")
+	if err != nil {
+		fmt.Println("Error reading config file:", err)
+		return
+	}
+	fmt.Println("Config:", string(config))
 }
 
 func main() {
@@ -21,7 +27,9 @@ func main() {
 	}
 
 	fmt.Println("Config:", string(config))
-
+	fmt.Println(os.Getenv("POD_NAME"))
+	fmt.Println("hello world")
+	fmt.Println("hello wochong")
 	fmt.Println("Server starting on port 8080...")
 	http.HandleFunc("/", handler)
 	http.ListenAndServe(":8080", nil)
